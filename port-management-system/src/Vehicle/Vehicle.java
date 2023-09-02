62b7177b4a46c7ce4cf0fa93d7ec3623a8ad2e5d
sunflowrr <s3978128@rmit.edu.vn>
2023-09-03 05:32:26 +0700

    Add Vehicle class properties and placeholders

--- port-management-system/src/Vehicle/Vehicle.java
+++ port-management-system/src/Vehicle/Vehicle.java
@@ -1,4 +1,48 @@
 package Vehicle;
 
+import Port.IPort;
+import Container.Container;
+import java.util.List;
+
 public class Vehicle implements IVehicle{
+    private String id;
+    private String name;
+    private double currentFuel;
+    private double carryingCapacity;
+    private double fuelCapacity;
+    private IPort currentPort;
+    private List<Container> containers;
+
+    public Vehicle() {
+    }
+
+    public Vehicle(String id, String name, double currentFuel, double carryingCapacity, double fuelCapacity, IPort currentPort) {
+        this.id = id;
+        this.name = name;
+        this.currentFuel = currentFuel;
+        this.carryingCapacity = carryingCapacity;
+        this.fuelCapacity = fuelCapacity;
+        this.currentPort = currentPort;
+    }
+
+    @Override
+    public void loadContainer(Container container) {
+    }
+
+    @Override
+    public void unloadContainer(Container container) {
+    }
+
+    @Override
+    public void moveTo(IPort port) {
+    }
+
+    @Override
+    public void refuel() {
+    }
+
+    @Override
+    public boolean canMoveTo(IPort port) {
+        return true; // Placeholder return value
+    }
 }
