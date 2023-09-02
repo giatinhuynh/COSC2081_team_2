d48d918b800750baa5aa4f9b84e86c1a6b4adfe6
sunflowrr <s3978128@rmit.edu.vn>
2023-09-03 05:21:31 +0700

    Update methods for IVehicle interface

--- port-management-system/src/Vehicle/IVehicle.java
+++ port-management-system/src/Vehicle/IVehicle.java
@@ -1,4 +1,17 @@
 package Vehicle;
 
+import Port.IPort;
+import Container.Container;
+
+// IVehicle interface
 public interface IVehicle {
+    void loadContainer(Container container);
+
+    void unloadContainer(Container container);
+
+    void moveTo(IPort port);
+
+    void refuel();
+
+    boolean canMoveTo(IPort port);
 }
