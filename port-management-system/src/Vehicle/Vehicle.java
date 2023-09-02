55f4c889c20331a160153e5e65c6c6573f656aa8
sunflowrr <s3978128@rmit.edu.vn>
2023-09-03 05:47:09 +0700

    Update Vehicle.java methods

--- port-management-system/src/Vehicle/Vehicle.java
+++ port-management-system/src/Vehicle/Vehicle.java
@@ -2,6 +2,8 @@ package Vehicle;
 
 import Port.IPort;
 import Container.Container;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Vehicle implements IVehicle{
@@ -12,6 +14,7 @@ public class Vehicle implements IVehicle{
     private double fuelCapacity;
     private IPort currentPort;
     private List<Container> containers;
+    private int totalContainers;
 
     public Vehicle() {
     }
@@ -23,26 +26,47 @@ public class Vehicle implements IVehicle{
         this.carryingCapacity = carryingCapacity;
         this.fuelCapacity = fuelCapacity;
         this.currentPort = currentPort;
+        this.containers = new ArrayList<>();
+        this.totalContainers = 0;
     }
 
     @Override
     public void loadContainer(Container container) {
+        if (canLoadContainer(container)) {
+            containers.add(container);
+            totalContainers++;
+        }
     }
 
     @Override
     public void unloadContainer(Container container) {
+        if (containers.contains(container)) {
+            containers.remove(container);
+            totalContainers--;
+        }
     }
 
     @Override
     public void moveTo(IPort port) {
+        if (canMoveTo(port)) {
+            currentPort = port;
+        }
     }
 
     @Override
     public void refuel() {
+        currentFuel = fuelCapacity;
     }
 
     @Override
     public boolean canMoveTo(IPort port) {
-        return true; // Placeholder return value
+        // Vehicle needs enough fuel to move to another port
+        return true;
+    }
+
+    public boolean canLoadContainer(Container container) {
+        // Ship can load any container, while truck can only load some containers
+        // Truck can only load to "landing" ports
+        return true;
     }
 }
