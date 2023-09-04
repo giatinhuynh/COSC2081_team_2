825190aabce45bd73b045708b3e4d322fd860cd1
Nhat Minh Phan <s3978598@rmit.edu.vn>
2023-09-04 19:22:17 +0700

    Basic interface structure for Port Manager

--- port-management-system/src/User/IPortManager.java
+++ port-management-system/src/User/IPortManager.java
@@ -1,4 +1,9 @@
 package User;
 
-public interface IPortManager extends IUser{
+public interface IPortManager extends IUser {
+    void createContainer(Container container);
+    Container readContainer(String id);
+    void updateContainer(Container container);
+    void deleteContainer(String id);
+    List<Trip> listTrips();
 }
