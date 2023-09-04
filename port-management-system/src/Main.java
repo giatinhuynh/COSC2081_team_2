426901be50409ed6480944017e5973d82de94211
Nhat Minh Phan <s3978598@rmit.edu.vn>
2023-09-04 13:18:55 +0700

    Add members information

--- port-management-system/src/Main.java
+++ port-management-system/src/Main.java
@@ -13,10 +13,10 @@ public class Main {
         System.out.println("COSC2081 GROUP ASSIGNMENT");
         System.out.println("CONTAINER PORT MANAGEMENT SYSTEM");
         System.out.println("Instructor: Mr. Minh Vu & Dr. Phong Ngo");
-        System.out.println("Group: TeamHi");
-        System.out.println("sXXXXXXX, Student Name");
-        System.out.println("sXXXXXXX, Student Name");
-        System.out.println("sXXXXXXX, Student Name");
-        System.out.println("sXXXXXXX, Student Name");
+        System.out.println("Group: Team Hi");
+        System.out.println("s3962053, Huynh Duc Gia Tin");
+        System.out.println("s3978598, Phan Nhat Minh");
+        System.out.println("s3978128, Nguyen Viet Ha");
+        System.out.println("s3978681, Vu Minh Ha");
     }
 }
