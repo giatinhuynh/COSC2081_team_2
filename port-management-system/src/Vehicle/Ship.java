a965bc19eb8d80c5f07c484b936cbc950af9f08a
sunflowrr <s3978128@rmit.edu.vn>
2023-09-04 21:17:50 +0700

    Update Ship.java

--- port-management-system/src/Vehicle/Ship.java
+++ port-management-system/src/Vehicle/Ship.java
@@ -3,7 +3,7 @@ package Vehicle;
 import Port.IPort;
 
 public class Ship extends Vehicle {
-    public Ship(String id, String name, double currentFuel, double carryingCapacity, double fuelCapacity, IPort currentPort) {
-        super(id, name, currentFuel, carryingCapacity, fuelCapacity, currentPort);
+    public Ship(String id, String name, double carryingCapacity, double fuelCapacity) {
+        super(id, name, carryingCapacity, fuelCapacity);
     }
 }
