bbd3635877befcf0408186c59dbef4dcae6c532f
sunflowrr <s3978128@rmit.edu.vn>
2023-09-04 21:17:44 +0700

    Update Truck.java

--- port-management-system/src/Vehicle/Truck.java
+++ port-management-system/src/Vehicle/Truck.java
@@ -1,34 +1,58 @@
 package Vehicle;
 
 import Port.IPort;
+import java.util.Scanner;
 import Container.Container;
 
-enum TruckType {
-    // Placeholder enum
-    BASIC, REEFER, TANKER
-}
-
 public class Truck extends Vehicle {
-    private TruckType truckType;
+    private boolean isTanker;
+    private boolean isReefer;
 
-    public Truck(String id, String name, double currentFuel, double carryingCapacity, double fuelCapacity, IPort currentPort, TruckType truckType) {
-        super(id, name, currentFuel, carryingCapacity, fuelCapacity, currentPort);
-        this.truckType = truckType;
+    public Truck(String id, String name, double carryingCapacity, double fuelCapacity, boolean isTanker, boolean isReefer) {
+        super(id, name, carryingCapacity, fuelCapacity);
+        this.isTanker = false;
+        this.isReefer = false;
+
+        // Ask for user input for truck type
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the type of truck: (1 for basic, 2 for reefer, 3 for tanker)");
+        int truckType = scanner.nextInt();
+        while (truckType != 1 && truckType != 2 && truckType != 3) {
+            System.out.println("Invalid input, please enter again: (1 for basic, 2 for reefer, 3 for tanker)");
+            truckType = scanner.nextInt();
+        }
+        if (truckType == 2) {
+            this.isReefer = true;
+        } else if (truckType == 3) {
+            this.isTanker = true;
+        }
     }
 
-    //Need to implement container type for checking
-    @Override
+    // Truck can only load containers that are allowed to be loaded on trucks
     public boolean canLoadContainer(Container container) {
-        // Check if truck can load the container type
-        if (truckType == TruckType.BASIC) {
-            // Basic trucks can carry dry storage, open top, and open side containers
+        // Use checkPort to check if the port is a landing port
+        if (!checkPort(this.getCurrentPort())) {
+            return false;
+        }
+
+        /* A basic truck can only carry dry storage, open top, and open side containers, while to carry
+        refrigerated containers we need to use reefer trucks, and to carry liquid containers we need to use
+        tanker trucks. */
+        if (!isReefer && !isTanker) { // Use getType from container
             return container.getType().equals("Dry Storage") || container.getType().equals("Open Top") || container.getType().equals("Open Side");
-        } else if (truckType == TruckType.REEFER) {
-            // Reefer trucks can carry refrigerated containers
+        } else if (isReefer) {
             return container.getType().equals("Refrigerated");
-        } else if (truckType == TruckType.TANKER) {
-            // Tanker trucks can carry liquid containers
+        } else {
             return container.getType().equals("Liquid");
+        }
+        return false;
+    }
+
+    // Helper method to check if the port is a landing port
+    private boolean checkPort(IPort currentPort) {
+        // Only the ports that are marked “landing” can utilize trucks for carrying
+        if (currentPort.isLanding()) { // Use isLanding from port
+            return true;
         }
         return false;
     }
