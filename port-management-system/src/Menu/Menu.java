a043fe0f69637882aac9f5f11a328a4217b6b22c
Nhat Minh Phan <s3978598@rmit.edu.vn>
2023-09-04 20:21:24 +0700

    Create Menu package and class to store all the menu screens

---
+++ port-management-system/src/Menu/Menu.java
@@ -0,0 +1,16 @@
+package Menu;
+
+public class Menu {
+    public static void onboardScreen() {
+        System.out.println("==========================================");
+        System.out.println("COSC2081 GROUP ASSIGNMENT");
+        System.out.println("CONTAINER PORT MANAGEMENT SYSTEM");
+        System.out.println("Instructor: Mr. Minh Vu & Dr. Phong Ngo");
+        System.out.println("Group: Team Hi");
+        System.out.println("s3962053, Huynh Duc Gia Tin");
+        System.out.println("s3978598, Phan Nhat Minh");
+        System.out.println("s3978128, Nguyen Viet Ha");
+        System.out.println("s3978681, Vu Minh Ha");
+        System.out.println("==========================================");
+    }
+}
