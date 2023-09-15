70f5dc2abb82db314ab3e163e34d0e65b8353f30
Nhat Minh Phan <s3978598@rmit.edu.vn>
2023-09-15 18:32:56 +0700

    Refine constants

--- src/utils/Constants.java
+++ src/utils/Constants.java
@@ -1,15 +1,37 @@
 package utils;
 
+import java.io.File;
 import java.util.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class Constants {
 
-    // File paths
-    public static final String CONTAINER_FILE_PATH = "src/database/data/containers.ser";
-    public static final String PORT_FILE_PATH = "src/database/data/ports.ser";
-    public static final String VEHICLE_FILE_PATH = "src/database/data/vehicles.ser";
-    public static final String TRIP_FILE_PATH = "src/database/data/trips.ser";
-    public static final String USER_FILE_PATH = "src/database/data/users.ser";
+    // Root directory for serialized files
+    private static final String ROOT_DIR = "src" + File.separator + "database" + File.separator + "data" + File.separator;
 
-    public static Date SYSTEM_DATE = new Date("2020/01/01 00:00:00");
+    // File paths
+    private static final String CONTAINER_FILE = "containers.ser";
+    private static final String PORT_FILE = "ports.ser";
+    private static final String VEHICLE_FILE = "vehicles.ser";
+    private static final String TRIP_FILE = "trips.ser";
+    private static final String USER_FILE = "users.ser";
+
+    // Build full paths using File.separator
+    public static final String CONTAINER_FILE_PATH = ROOT_DIR + CONTAINER_FILE;
+    public static final String PORT_FILE_PATH = ROOT_DIR + PORT_FILE;
+    public static final String VEHICLE_FILE_PATH = ROOT_DIR + VEHICLE_FILE;
+    public static final String TRIP_FILE_PATH = ROOT_DIR + TRIP_FILE;
+    public static final String USER_FILE_PATH = ROOT_DIR + USER_FILE;
+
+    public static final Date SYSTEM_DATE = initializeSystemDate();
+
+    // Initialize SYSTEM_DATE using a non-deprecated method
+    private static Date initializeSystemDate() {
+        try {
+            return new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").parse("2020/01/01 00:00:00");
+        } catch (ParseException e) {
+            throw new RuntimeException("Failed to parse system date", e);
+        }
+    }
 }
