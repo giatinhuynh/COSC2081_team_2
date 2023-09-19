1cc4e35a9c90151f2e06c10071355bc0c9577f51
Nhat Minh Phan <s3978598@rmit.edu.vn>
2023-09-19 18:23:08 +0700

    Remove unused interfaces

--- src/interfaces/ICRUD.java
+++
@@ -1,9 +0,0 @@
-package interfaces;
-
-public interface ICRUD {
-    public void create();
-    public void displayOne();
-    public void displayAll();
-    public void update();
-    public void delete();
-}
