6a039c26dc5aa2114297f1a21341eb81320d2d14
Nhat Minh Phan <s3978598@rmit.edu.vn>
2023-09-20 23:51:45 +0700

    Update ManagerVehicleInterface.java

--- src/interfaces/manager/ManagerVehicleInterface.java
+++ src/interfaces/manager/ManagerVehicleInterface.java
@@ -1,4 +1,8 @@
 package interfaces.manager;
 
+import java.text.ParseException;
+
 public interface ManagerVehicleInterface extends ManagerInterface {
+    public void refuelVehicle();
+    public void deployVehicle() throws ParseException;
 }
