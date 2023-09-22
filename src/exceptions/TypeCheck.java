634232eabe15fb6910b8ece0fe16bf47a2645599
Nhat Minh Phan <s3978598@rmit.edu.vn>
2023-09-22 23:52:51 +0700

    Class to check the type of the input

---
+++ src/exceptions/TypeCheck.java
@@ -0,0 +1,20 @@
+package exceptions;
+
+public class TypeCheck {
+    public boolean isDouble(String input) {
+        try {
+            Double.parseDouble(input);
+            return true;
+        } catch (NumberFormatException ex) {
+            return false;
+        }
+    }
+
+    public boolean isBoolean(String input) {
+        return input.equalsIgnoreCase("true") || input.equalsIgnoreCase("false");
+    }
+
+    public boolean isString(String input) {
+        return !isDouble(input) && !isBoolean(input);
+    }
+}
