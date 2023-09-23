69ffa2c5487da2e6666796ed3b9b76ff7ba451cc
Nhat Minh Phan <s3978598@rmit.edu.vn>
2023-09-23 08:07:07 +0700

    Add necessary method for the flow

--- src/views/flow/PortManagerFlow.java
+++ src/views/flow/PortManagerFlow.java
@@ -42,22 +42,24 @@ public class PortManagerFlow extends BaseView {
         displayMessage("1. Load Container");
         displayMessage("2. Unload Container");
         displayMessage("===================================");
-        displayMessage("3. Add Container");
-        displayMessage("4. Update Container");
-        displayMessage("5. Delete Container");
-        displayMessage("6. Search Container");
-        displayMessage("7. View All Containers");
+        displayMessage("3. Add Existing Container");
+        displayMessage("4. Add New Container");
+        displayMessage("5. Update Container");
+        displayMessage("6. Delete Container");
+        displayMessage("7. Search Container");
+        displayMessage("8. View All Containers");
         displayMessage("0. Back");
 
         int choice = promptForInput("Enter you choice: ");
         switch (choice) {
             case 1 -> containerServices.loadContainerFlow();
             case 2 -> containerServices.unloadContainerFlow();
-            case 3 -> containerServices.createNewContainer();
-            case 4 -> containerServices.updateContainer();
-            case 5 -> containerServices.deleteContainer();
-            case 6 -> containerServices.findContainer();
-            case 7 -> containerServices.displayAllContainers();
+            case 3 -> containerServices.addExistingContainer();
+            case 4 -> containerServices.createNewContainer();
+            case 5 -> containerServices.updateContainer();
+            case 6 -> containerServices.deleteContainer();
+            case 7 -> containerServices.findContainer();
+            case 8 -> containerServices.displayAllContainers();
             case 0 -> PortManagerMenu();
             default -> {
                 displayMessage("Invalid choice. Please try again.");
